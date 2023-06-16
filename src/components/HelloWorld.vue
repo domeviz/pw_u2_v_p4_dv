ff20e5fc3cf504a3112b326a5082bb3d74aadbb1
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-15 19:46:31 -0500

    Taller 17

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="hello">
+    <input type="text">
+    <button>Aceptar</button>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
