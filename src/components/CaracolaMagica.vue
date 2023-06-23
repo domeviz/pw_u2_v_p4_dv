1f28a9a393f36faedbfb57ca19e91fe79e93d981
Domenica Vizcarra <domeviz3410@gmail.com>
2023-06-22 19:46:43 -0500

    Taller 21

--- src/components/CaracolaMagica.vue
+++ src/components/CaracolaMagica.vue
@@ -1,24 +1,79 @@
 <template>
   <h1>Caracola Mágica</h1>
-  <img src="https://play-lh.googleusercontent.com/0mgvhYfhG4Qm7UsE8uW4P2SUBw-ujHOTAgcP8DpXUafktzlUza2MRzLDK02AUK4cYZxT" alt="No se puede presentar">
-  <input v-model="pregunta" type="text" placeholder="Hazme una pregunta">
-  <p>Recuerda terminar con un enter la pregunta</p>
-<div>
-    <h2>Voy a pasar el año?</h2>
-    <h1>SI, NO</h1>
-</div>
+  <img :src="rutaImagen" alt="No se puede presentar" />
+  <div class="bg-dark"></div>
+  <div class="contenedor">
+    <input v-model="pregunta" type="text" placeholder="Hazme una pregunta" />
+    <p>Recuerda terminar con un ? la pregunta</p>
+    <div>
+      <h2>{{ pregunta }}</h2>
+      <h1>{{ respuesta }}</h1>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            pregunta: 'Hola Mundo'
-        }
-    }
-}
+  data() {
+    return {
+      pregunta: "",
+      respuesta: "",
+      rutaImagen:
+        "https://play-lh.googleusercontent.com/0mgvhYfhG4Qm7UsE8uW4P2SUBw-ujHOTAgcP8DpXUafktzlUza2MRzLDK02AUK4cYZxT",
+    };
+  },
+  watch: {
+    pregunta(value, oldValue) {
+      console.log(value);
+      console.log(oldValue);
+      if (value.includes("?")) {
+        console.log("Consumir el API");
+        this.consumirAPI();
+      }
+    },
+  },
+  methods: {
+    async consumirAPI() {
+      const { answer, image } = await fetch("https://yesno.wtf/api").then((r) =>
+        r.json()
+      );
+      console.log(answer);
+      console.log(image);
+      this.respuesta = answer;
+      this.rutaImagen = image;
+    },
+  },
+};
 </script>
 
 <style>
-
+img,
+.bg-dark {
+  height: 100vh;
+  width: 100vw;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+}
+.bg-dark {
+  background-color: rgb(0, 0, 0, 0.4);
+}
+.contenedor {
+  position: relative;
+}
+input {
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: none;
+}
+p,h1,h2{
+    color: white;
+}
+p{
+    font-size: 20px;
+    margin-top: 0px;
+}
 </style>
