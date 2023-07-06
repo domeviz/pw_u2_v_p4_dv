ed5578598ccafcfbe1bb3ef2c2b72fa1dbfdf997
Domenica Vizcarra <domeviz3410@gmail.com>
2023-07-06 11:27:59 -0500

    Tarea 10: Juego Pokemon

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -2,12 +2,18 @@
   <h1 v-if="!pokemonCorrecto">Espere por favor...</h1>
   <div v-else>
     <h1>Juego Pokemon</h1>
+    <h2>Intentos: {{ intentos }}</h2>
     <PokemonImg :pokemonId="pokemonCorrecto.id" :muestraPokemon="showPokemon" />
     <PokemonOps
       :opciones="pokemonArr"
       @seleccionado="revisarSeleccion($event)"
     />
   </div>
+  <div v-if="mostrarMensaje">
+      <h2 v-if="puntaje>0">Ganaste! Puntaje: {{ puntaje }}</h2>
+      <h2 v-else>Perdiste!</h2>
+      <button @click="reiniciarJuego">Reiniciar Juego</button>
+    </div>
 </template>
 
 <script>
@@ -26,6 +32,10 @@ export default {
       pokemonArr: [],
       pokemonCorrecto: null,
       showPokemon: false,
+      puntaje:0,
+      intentos:0,
+      juegoTerminado:false,
+      mostrarMensaje:false
     };
   },
   methods: {
@@ -41,15 +51,42 @@ export default {
       console.log(indicePokemon)
     },
     revisarSeleccion(idSeleccionado) {
+      if(this.juegoTerminado) return;
       console.log("evento en el padre");
       this.showPokemon = true;
+      this.intentos++;
       console.log(idSeleccionado);
+      console.log("Intentos")
+      console.log(this.intentos)
       if(idSeleccionado==this.pokemonCorrecto.id){
-        console.log("ganaste")
+        if(this.intentos===1){
+          this.puntaje+=5;
+        } else if(this.intentos===2){
+          this.puntaje+=2;
+        } else if(this.intentos===3){
+          this.puntaje+=1;
+        }
+        console.log("Ganaste");
+        console.log("Puntaje: ",this.puntaje);
+        this.mostrarMensaje=true;
+        this.juegoTerminado=true;
       } else{
-        console.log("perdiste")
+        if(this.intentos===3){
+          console.log("Perdiste");
+          this.mostrarMensaje=true;
+          this.juegoTerminado=true;
+        }
       }
     },
+    reiniciarJuego(){
+      this.pokemonCorrecto=null;
+      this.showPokemon=false;
+      this.puntaje=0;
+      this.intentos=0;
+      this.juegoTerminado=false;
+      this.mostrarMensaje=false;
+      this.cargaJuegoInicial();
+    }
   },
   mounted() {
     console.log("Se monto el componente");
